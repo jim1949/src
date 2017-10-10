27dd5a49ce60c6df7d2b22393c21e33f73d6ecfb
jim1949 <jim1949@163.com>
2017-10-10 09:45:12 +0800

    add newest changes to my api_ws.

--- operation/src/mapping/mapsaver_pause.sh
+++ operation/src/mapping/mapsaver_pause.sh
@@ -12,3 +12,4 @@ cp $1/now/map.pgm $1/$2/$2.pgm
 cp $1/now/map.yaml $1/$2/$2.yaml 
 rosrun map_server store_img.py $1 $2 $3 
 # kill -9 `ps -x | grep tf_learning|grep -v grep |  awk '{print $1}'`
+echo map saved to $1/$2/$2.pgm
