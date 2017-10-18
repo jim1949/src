45f3607b4bd4ecc6821e8f0c17e48ac65615daaf
vcdanda <jim1949@163.com>
2017-10-18 14:53:31 +0800

    update the robot_path_reader

---
+++ operation/src/robot_path_reader.cpp
@@ -0,0 +1,45 @@
+// g++ -std=c++11 robot_path_reader.cpp  -o robot_path_reader
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#define Pi 3.1415926
+
+
+using namespace std;
+
+int main(){
+    fstream myfile("ppl_src/robot_path.txt", ios_base::in);
+    ofstream ofile;
+    ofile.open("/home/relaybot/api_ws/src/operation/src/ppl_src/robot_path.txt");
+    float ft_line;
+	int num;
+    int count = 0;
+    if(myfile.is_open())
+	{
+        cout << " txt opened" << endl;
+		myfile >> ft_line;
+		num=ft_line;
+		cout<<"num:"<<num<<endl;
+		for(int i=0;i<num;i++)
+        {   printf("\nrow %d: ",i);
+			for(int j=0;j<4;j++){
+                
+                myfile>>ft_line;
+                printf(" column %d: %f",j,ft_line);
+		    }
+            //x,y,w,z
+
+            for(i = 0; i < robot_path.size(); ++i){
+            ofile << robot_path[i].x << " " << robot_path[i].y << " " << w[i] << " " << z[i] << endl;
+    }
+    
+        }
+        cout<<endl;		
+    }
+    myfile.close();
+    ofile.close();
+
+
+    return 0;
+}
